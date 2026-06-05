00c686faf51948f63c2a52aa66222b802ec3f1dc
pitamber <pitamber.rawat@sourceinfosys.com>
2026-06-05 19:30:32 +0530

    updated code

--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -1,2 +1,9 @@
 const jwt = require('jsonwebtoken');
 
+const JWT_SECRET = "sdafnsdkfsndfsdfsdf[sdf[sdf[s]dfs]df"
+
+const generateToken = (userId)=>{
+    return jwt.sign({userId}, JWT_SECRET, {expiresIn:"1d"})
+}
+
+module.exports = generateToken;
